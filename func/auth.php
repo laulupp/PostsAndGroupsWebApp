34f617743d23d44be2f20fded17748c70e8688f1
laulupp <lupp.laurentiu@gmail.com>
2022-11-25 13:33:14 +0200

    update auth.php

--- func/auth.php
+++ func/auth.php
@@ -7,6 +7,9 @@ function checkUserExists($user){
     return false;
 }
 function tryToLogin($user, $password){
+    if($user === null || $password === null){
+        return "Invalid creditentials";
+    }
     $res = pg_query("SELECT password FROM ".$db_name." WHERE username = ".$user);
     if(pg_num_rows($res) > 0){
         $pwd = pg_fetch_row($res);
@@ -16,12 +19,25 @@ function tryToLogin($user, $password){
             $_SESSION['user'] = $user;
             $_SESSION['password'] = $password;
             return "";
-        } else {
-            $_auth=false;
-            return "Invalid password";
         }
+
+        $_auth=false;
+        return "Invalid password";
     }
     return "Invalid username";
 }
+function tryToRegister($user, $password, $repassword){
+    if($user === null || $password === null || $repassword === null){
+        return "Please complete all fields";
+    }
+    if(checkUserExists($user)){
+        return "The username already exists";
+    }
 
+    if($password != $repassword){
+        return "The passwords don't match"
+    }
+    pg_query("INSERT INTO ".$db_name."(username, password) VALUES (".$user.", ".md5($password).")");
+    return "";
+}
 ?>
