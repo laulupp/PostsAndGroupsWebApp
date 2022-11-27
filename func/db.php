1a7a31668c2d9a121c03bfa513f72cc40998e34c
laulupp <104854345+laulupp@users.noreply.github.com>
2022-11-28 00:05:05 +0200

    Update db.php

--- func/db.php
+++ func/db.php
@@ -5,7 +5,7 @@ function dbConnect($dbC){
         $connection = pg_connect("host=".$dbC->db_host." port=".$dbC->db_port." user=".$dbC->db_username." password=".$dbC->db_password);
         if($connection){
             if(pg_num_rows(pg_query("SELECT datname FROM pg_database WHERE datname = '".$dbC->db_name."'")) == 0){
-                pg_query("CREATE DATABASE ".dbC->$db_name);
+                pg_query("CREATE DATABASE ".$dbC->db_name);
             }
             $connection = pg_connect("host=".$dbC->db_host." port=".$dbC->db_port." dbname=".$dbC->db_name." user=".$dbC->db_username." password=".$dbC->db_password);
             pg_query("CREATE SCHEMA IF NOT EXISTS ".$dbC->db_schema." AUTHORIZATION CURRENT_ROLE");
