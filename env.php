e0bd6272a5b017e2d7f9749d43e0184d7bb9c7e6
laulupp <lupp.laurentiu@gmail.com>
2023-01-05 17:06:53 +0200

    update env.php and db.php

--- env.php
+++ env.php
@@ -10,6 +10,13 @@
             public $db_schema     = "public";
             public $db_usersTable = "users";
             public $db_postsTable = "posts";
+            public $db_replyTable = "reply";
+            public $db_sportsTable = "sports";
+            public $db_electronicsTable = "electronics";
+            public $db_entertainmentTable = "entertainment";
+            public $db_resolver = "resolver";
+            public $db_group = "group";
+            public $db_messages = "messages";
       }
 
 ?>
--- func/db.php
+++ func/db.php
@@ -9,8 +9,15 @@ function dbConnect($dbC){
             }
             $connection = pg_connect("host=".$dbC->db_host." port=".$dbC->db_port." dbname=".$dbC->db_name." user=".$dbC->db_username." password=".$dbC->db_password);
             pg_query("CREATE SCHEMA IF NOT EXISTS ".$dbC->db_schema." AUTHORIZATION CURRENT_ROLE");
-            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_usersTable."( id SERIAL primary key, username Varchar(255) not null, password Varchar(255) not null)");
-            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_postsTable."( id SERIAL primary key, owner Varchar(255) not null, title Varchar(255) not null, shortdescription Varchar(255) not null, longdescription Varchar(5000), datecreated timestamp NOT NULL DEFAULT now())");
+            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_usersTable."( id SERIAL primary key, username Varchar(255) not null, password Varchar(255) not null, fullname Varchar(255) not null)");
+            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_postsTable."( id SERIAL primary key, owner Varchar(255) not null, title Varchar(255) not null, shortdescription Varchar(255) not null, datecreated timestamp NOT NULL DEFAULT now(), type Integer not null, idoftype Integer not null)");
+            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_replyTable."( id SERIAL primary key, postid Integer not null, message Varchar(1000) not null, ownerid Integer not null )");
+            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_sportsTable."( id SERIAL primary key not null, team1 Varchar(255) not null, team2 Varchar(255) not null, winner Varchar(255) not null, Score Varchar(255) not null )");
+            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_electronicsTable."( id SERIAL primary key not null, components Varchar(1000) not null, mountingtutorial Varchar(2500) not null )");
+            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_entertainmentTable."( id SERIAL primary key not null, joke Varchar(1000) not null, source Varchar(255) not null )");
+            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_resolver."( id SERIAL primary key not null, userid Integer not null, groupid Integer not null)");
+            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_group."( id SERIAL primary key not null, groupkey Integer not null)");
+            pg_query("CREATE TABLE IF NOT EXISTS ".$dbC->db_schema.".".$dbC->db_messages."( id SERIAL primary key not null, groupid Integer not null, ownerid Integer not null, text Varchar(500) not null)");
             return true;
         }
         echo "No DB connection";
